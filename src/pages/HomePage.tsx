b84e9dbdbf51f56c158ff5272da7c46d896f9659
skyworth-web <devskyworth03@gmail.com>
2025-06-02 21:35:43 -0700

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,7 +11,7 @@ import CTASection from '../components/home/CTASection';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="flex flex-col h-screen overflow-hidden">
+    <div className="flex flex-col h-screen">
       <Navbar />
       <main className="flex-grow">
         <HeroSection userType="freelancer" />
